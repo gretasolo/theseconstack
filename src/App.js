0e501a59dccef572f49131c2e19f13ffe7737f53
Greta Solomon <gs2806@nyu.edu>
2019-09-25 12:48:23 +0800

    Changed the HTML

--- src/App.js
+++ src/App.js
@@ -5,22 +5,50 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <title>Stack</title>
+    <p><strong> "Facebook Community Operations"</strong></p>
+    <link href="https://fonts.googleapis.com/css?family=Press+Start+2P&display=swap" rel="stylesheet">
+
+
+<div class= "rotating-box">
+<div class="single-rb">
+<div class="front-side"> <div>
+<img src= "https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1440,w_2560,x_0,y_0/dpr_1.5/c_limit,w_608/fl_lossy,q_auto/v1551106961/RTR2X79V_1_xbt5mp
+" alt="" />height=800px </div></div></div><p>user: facebook moderator employees sorting through uploaded content</p>
+</div> 
+
+<div class="back-side">
+<a href="https://imgur.com/FwCvqnv"><img src="https://i.imgur.com/FwCvqnv.png" title="source: imgur.com" alt="" height {800px} /> </a>
+<p> interface: facebook web platform </p>
+</div>
+
+<div class= "left-side">
+<a href="https://imgur.com/8ZRNqjt"><img src="https://i.imgur.com/8ZRNqjt.png" alt= "" title="source: imgur.com" height {800px} /> </a>
+<p>address: facebook's community operations centers</p>
+</div>
+
+<div class= "right-side">
+<img src="https://socialmediaweek.org/wp-content/blogs.dir/1/files/2015/02/Facebook-Servers-3rd-Image.jpg" alt="" height {800px} />
+<p> city: the servers with the uploaded content</p>
+</div>
+
+<div class= "top-side">
+<img src="https://engineering.fb.com/wp-content/uploads/2017/12/gdpqcgfewfsseq0iaaaaaadhlmn0bj0jaaab.jpg" alt="" height {800px} />
+<p>cloud: the data centers with the central storage</p>
+</div>
+
+<div class= "bottom-side">
+<img src="https://gigaom.com/wp-content/uploads/sites/1/2010/12/facebook-connection-map-3x2.png" alt="" height {800px} />
+<p>earth: physical storage with the public and delted data from users and viewers</p>
+</div >
+
+
+
     </div>
+    
   );
 }
 
 export default App;
+
+
